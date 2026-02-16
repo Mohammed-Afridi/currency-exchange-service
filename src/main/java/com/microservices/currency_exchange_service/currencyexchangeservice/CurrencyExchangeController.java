5ee369fe2f1a7199502172dde917eb85e9f220f7
mohammed_git <mohammedafridi62@gmail.com>
2026-02-17 01:12:40 +0530

    add port using Environemnt

--- src/main/java/com/microservices/currency_exchange_service/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/microservices/currency_exchange_service/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.microservices.currency_exchange_service.currencyexchangeservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,16 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
         CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
-        currencyExchange.setEnvironment("8080"); // Where do i get this port from,
-        // Spring provided environemtn class
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port); // Where do i get this port from,
+        // Spring provided environemtn class - the above line getProperty
         return currencyExchange;
     }
 }
